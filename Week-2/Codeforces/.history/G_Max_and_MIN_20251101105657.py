ba77688f26fe7273989a01081fb0b0e701f87e2b
rabeyanoor <rabeyanoor.dev@gmail.com>
2025-11-01 11:00:15 +0600

    find minimum and maximum value

---
+++ Week-2/Codeforces/.history/G_Max_and_MIN_20251101105657.py
@@ -0,0 +1,5 @@
+def find_min_max(arr):
+    minimum =min(arr)
+    maximum= max(arr)
+    print(minimum,maximum)
+    
---
+++ Week-2/Codeforces/.history/G_Max_and_MIN_20251101105744.py
@@ -0,0 +1,9 @@
+def find_min_max(arr):
+    minimum =min(arr)
+    maximum= max(arr)
+    print(minimum,maximum)
+
+n=int(input())
+a=list(map(intc,input().split()))
+
+find
